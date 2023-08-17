f61b5da819016bf356ab2cb92b27a6e610002913
guoguangwu <guoguangwu@magic-shield.com>
2023-08-17 19:02:51 +0800

    fix typo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # grumpy
 
-Kubernetes Validation Admission Controller example. I have writen down [a guide explaining how it is has been built and how to run it](https://docs.giantswarm.io/guides/creating-your-own-admission-controller).
+Kubernetes Validation Admission Controller example. I have written down [a guide explaining how it is has been built and how to run it](https://docs.giantswarm.io/guides/creating-your-own-admission-controller).
--- grumpy.go
+++ grumpy.go
@@ -65,7 +65,7 @@ func (gs *GrumpyServerHandler) serve(w http.ResponseWriter, r *http.Request) {
 		glog.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
 	}
-	glog.Infof("Ready to write reponse ...")
+	glog.Infof("Ready to write response ...")
 	if _, err := w.Write(resp); err != nil {
 		glog.Errorf("Can't write response: %v", err)
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
